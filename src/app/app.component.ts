4c0b45314a7947260ca0b8ed6f69b925eea5d0e9
User2 <User2@RDV-004834.hylandqa.net>
2019-03-06 18:23:44 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+
+
+
+class Class {
+  constructor(name1:string, numberOfNotes:number ){
+
+  }
+}
+
+export class AppComponent  {
+  name = 'Angular';
+
+}
